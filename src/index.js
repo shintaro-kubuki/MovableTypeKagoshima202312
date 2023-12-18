fb793abb5e4e8d65cd203193c4563477175a7e18
shintaro-kubuki <kanse.net@gmail.com>
2023-12-18 18:08:21 +0900

    修正

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './styles.scss';
+
+function openModal() {
+    console.log("openModal")
+    const myModal = new bootstrap.Modal(document.getElementById('myModal'), options)
+    myModal.toggle();
+}
